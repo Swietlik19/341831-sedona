b6e25dadb4d9c6bd3342103bc1030fd5d5e80d36
Lyubov Mukhacheva <citymnk@gmail.com>
2017-06-25 02:31:23 +0700

    Поправила ошибки по критериям

--- js/script.js
+++ js/script.js
@@ -7,15 +7,6 @@ var popupSuccess = document.querySelector(".modal--success");
 
 var form = document.querySelector(".form-block__form");
 
-var inputName = form.querySelector("[name = name]");
-var inputSurname = form.querySelector("[name = surname]");
-var inputEmail = form.querySelector("[name = email]");
-var inputTelephone = form.querySelector("[name = telephone]");
-
-var modalErrorClose = popupError.querySelector(".modal-btn");
-var modalSuccessClose = popupSuccess.querySelector(".modal-btn");
-
-
 menuBlock.classList.remove("main-nav__list--open");
 
 gamburgerButton.addEventListener("click", function () {
@@ -30,48 +21,58 @@ crossButton.addEventListener("click", function () {
 
 var errorCheck = false;
 
-form.addEventListener("submit", function (evt) {
-  if (!inputName.value) {
-    errorCheck = true;
-    inputName.classList.add("form-block__user-info--error");
-  } else {
-    inputName.classList.remove("form-block__user-info--error");
-  }
+if (form) {
+  var inputName = form.querySelector("[name = name]");
+  var inputSurname = form.querySelector("[name = surname]");
+  var inputEmail = form.querySelector("[name = email]");
+  var inputTelephone = form.querySelector("[name = telephone]");
+  var modalErrorClose = popupError.querySelector(".modal-btn");
+  var modalSuccessClose = popupSuccess.querySelector(".modal-btn");
 
-  if (!inputSurname.value) {
-    errorCheck = true;
-    inputSurname.classList.add("form-block__user-info--error");
-  } else {
-    inputSurname.classList.remove("form-block__user-info--error");
-  }
+  form.addEventListener("submit", function (evt) {
+    if (!inputName.value) {
+      errorCheck = true;
+      inputName.classList.add("form-block__user-info--error");
+    } else {
+      inputName.classList.remove("form-block__user-info--error");
+    }
 
-  if (!inputEmail.value) {
-    errorCheck = true;
-    inputEmail.classList.add("form-block__user-info--error");
-  } else {
-    inputEmail.classList.remove("form-block__user-info--error");
-  }
+    if (!inputSurname.value) {
+      errorCheck = true;
+      inputSurname.classList.add("form-block__user-info--error");
+    } else {
+      inputSurname.classList.remove("form-block__user-info--error");
+    }
 
-  if (!inputTelephone.value) {
-    errorCheck = true;
-    inputTelephone.classList.add("form-block__user-info--error");
-  } else {
-    inputTelephone.classList.remove("form-block__user-info--error");
-  }
+    if (!inputEmail.value) {
+      errorCheck = true;
+      inputEmail.classList.add("form-block__user-info--error");
+    } else {
+      inputEmail.classList.remove("form-block__user-info--error");
+    }
 
-  if (errorCheck) {
-    evt.preventDefault();
-    popupError.classList.add("modal--show");
-  } else {
-    popupSuccess.classList.add("modal--show");
-  }
-});
+    if (!inputTelephone.value) {
+      errorCheck = true;
+      inputTelephone.classList.add("form-block__user-info--error");
+    } else {
+      inputTelephone.classList.remove("form-block__user-info--error");
+    }
 
-modalErrorClose.addEventListener("click", function () {
-  errorCheck = false;
-  popupError.classList.remove("modal--show");
-});
+    if (errorCheck) {
+      evt.preventDefault();
+      popupError.classList.add("modal--show");
+    } else {
+      popupSuccess.classList.add("modal--show");
+    }
+  });
 
-modalSuccessClose.addEventListener("click", function () {
-  popupSuccess.classList.remove("modal--show");
-});
+  modalErrorClose.addEventListener("click", function () {
+    errorCheck = false;
+    popupError.classList.remove("modal--show");
+  });
+
+  modalSuccessClose.addEventListener("click", function () {
+    popupSuccess.classList.remove("modal--show");
+  });
+
+}
